6df9f35d8fa1b35b8ffd7d017971a8201f5aacf5
TJ Hooker <tyrelhooker@gmail.com>
2020-01-27 21:51:46 -0500

    Create file structure. Create function to print table. Bug where outer loop in function only prints length of items, not the length of items items

---
+++ miscStringPrograms/tablePrinter/tablePrinter.py
@@ -0,0 +1,19 @@
+kennelFeedingData = [
+    ['Scarlet', 'Lucky', 'Lady', 'Dewey'],
+    ['Allison', 'James', 'LeAnn', 'Nick'],
+    ['1/2 Cup', '1 Cup', '3/4 Cup', '1 Cup']
+]
+
+def printTable(stringData):
+    # for i in range(len(stringData)):
+    #     print(f'start of i: {i}')
+    #     print(f'this is stringData[i]: {stringData[i]}')
+    #     for k in range(len(stringData)):
+    #         print(f'start of k: {k}')
+    #         print(stringData[i][k])
+    #         print('end of k')
+    #     print('end of i \n\n')
+
+
+
+printTable(kennelFeedingData)
