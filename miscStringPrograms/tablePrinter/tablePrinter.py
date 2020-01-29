d7efebcb5b13cbd288ecbc2bf611cd4b33021c5a
TJ Hooker <tyrelhooker@gmail.com>
2020-01-28 22:50:39 -0500

    Program for loop to determine length of each string and save largest length value. Program for loop to iterate through hard-coded table data and rearrange the table data and justify each value

--- miscStringPrograms/tablePrinter/tablePrinter.py
+++ miscStringPrograms/tablePrinter/tablePrinter.py
@@ -1,19 +1,59 @@
-kennelFeedingData = [
-    ['Scarlet', 'Lucky', 'Lady', 'Dewey'],
-    ['Allison', 'James', 'LeAnn', 'Nick'],
-    ['1/2 Cup', '1 Cup', '3/4 Cup', '1 Cup']
+kennel_feeding_data = [
+    ['Deputy Dewey', 'Lucky', 'Lady', 'Scarlett'],
+    ['Alli', 'James', 'LeAnn', 'Nick'],
+    ['1/2 Cup or 3/4 Cup', '1 Cup', '3/4 Cup', '1 Cup']
 ]
 
-def printTable(stringData):
-    # for i in range(len(stringData)):
-    #     print(f'start of i: {i}')
-    #     print(f'this is stringData[i]: {stringData[i]}')
-    #     for k in range(len(stringData)):
-    #         print(f'start of k: {k}')
-    #         print(stringData[i][k])
-    #         print('end of k')
-    #     print('end of i \n\n')
+def print_table(table_data):
+    column_width = [0] * len(table_data)
+
+    for row in range(len(table_data)):
+        column = row
+        for row_element in range(len(table_data[row])):
+            length_of_element = len(table_data[row][row_element])
+
+            if length_of_element > column_width[column]:
+                column_width[column] = length_of_element
+
+    for columns in range(len(table_data) + 1):
+        for rows in range(len(table_data)):
+            print(table_data[rows][columns].rjust(column_width[rows]),
+                  end=" " * 8)
+            # print(r_justified_text, end="")
+        print('')
 
 
 
-printTable(kennelFeedingData)
+
+        # print(f'columnWidth{i}: {columnWidth[i]}')
+        # for j in range(len(tableData)):
+        #     print(j)
+        #     lengthOfString = len(tableData[j][i])
+        #     print(tableData[j][i])
+        #     print(f'lengthOfString: {lengthOfString}')
+        #     if lengthOfString > columnWidth[j]:
+        #         columnWidth[j] = lengthOfString
+        #     # # print(f'{j}: {lengthOfString}')
+        #     # # print(tableData[j])
+        #     print(f'columnWidth{i}: {columnWidth[i]}')
+
+
+    # TODO for i in range(len(tableData) + 1):
+    #         for k in range(len(tableData)):
+    #             modifiedItem = tableData[k][i]
+    #             print(modifiedItem)
+
+
+
+
+
+
+
+    # for i in range(len(kennelFeedingData)):
+    #     outerLoop = kennelFeedingData[i]
+    #     # print(outerLoop)
+    #     for i2 in range(len(outerLoop)):
+    #         innerLoop = outerLoop[i2]
+    #         # print(f'InnerLoop: {innerLoop}')
+    #         print(kennelFeedingData[i], innerLoop)
+print_table(kennel_feeding_data)
