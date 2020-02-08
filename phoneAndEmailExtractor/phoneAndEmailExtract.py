3b04f2f8ebc4eeaefdee8704c40c648cd1ed947c
TJ Hooker <tyrelhooker@gmail.com>
2020-02-08 17:24:45 -0500

    Modify code, enabling list iterator for phone number regex

--- phoneAndEmailExtractor/phoneAndEmailExtract.py
+++ phoneAndEmailExtractor/phoneAndEmailExtract.py
@@ -9,10 +9,22 @@ phoneNumRegex = re.compile(r'''(
     (\d{3})
     (\s|-|\.)
     (\d{4})
-    )''', re.VERBOSE)
-mo = phoneNumRegex.finditer(str(samplePhoneNumbers))
+    )''', re.X)  # re.X is the same as re.VERBOSE
+
+mo = phoneNumRegex.findall(str(samplePhoneNumbers))
+print(type(mo))
+# print(type(mo) == list)
+if type(mo) == list:
+    phoneNumberList = ''
+    for i in mo:
+        phoneNumberList += f'{i[0]}\n'
+    print(phoneNumberList)
+else:
+    print(mo)
+
+# mo = phoneNumRegex.finditer(str(samplePhoneNumbers))
 # print(type(str(samplePhoneNumbers)))
-print(mo['match'])
+# print(mo['match'])
 
 # phoneNumRegex = re.compile(r'((\d{3}|\(\d{3}\)|\[\d{3}\])?(\s|-|\.)?(\d{'
 #                            r''r'3})(\s|-|\.)(\d{4}))', re.VERBOSE)
@@ -22,3 +34,9 @@ print(mo['match'])
 # # for i in range(len(mo)):
 # #     print(mo[i][0])
 # print(list(filter(phoneNumRegex.match, samplePhoneNumbers)))
+
+# for i in samplePhoneNumbers:
+#     mo = phoneNumRegex.findall(i)
+#     print(mo)
+#     # for item in mo:
+#     #     print(item[0])
