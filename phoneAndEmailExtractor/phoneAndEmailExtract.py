c6ff64272ac8091a40ecf79bb168d92026e71be8
TJ Hooker <tyrelhooker@gmail.com>
2020-02-10 00:04:30 -0500

    Refactor email address for loop. Create copy to clipboard logic. Clean up code.

--- phoneAndEmailExtractor/phoneAndEmailExtract.py
+++ phoneAndEmailExtractor/phoneAndEmailExtract.py
@@ -5,14 +5,10 @@
 import pyperclip
 import re
 
-samplePhoneNumbers = ['8900988900', '123 456-7890', '234-567-8901',
-                      '(345) 678-9012', '456.789.0123', '555-555-5555 ext.80',
-                      '555-555-5555', 'testing this to see what kind of error I am re']
-sampleEmailAddresses = 'This string contains several test@gmail.com and yahoo@yahoo.com email addresses. I am testing to make sure single @ and .com are not caught as well as yahoo@ and @blank.com are not captured. If this works properly then testing@aol.com will be captured. And everything in "thisEmail@email.com" will also be captured.'
-
-# Testing out the
-# turned of soft wrap
-
+# samplePhoneNumbers = ['8900988900', '123 456-7890', '234-567-8901',
+#                       '(345) 678-9012', '456.789.0123', '555-555-5555 ext.80',
+#                       '555-555-5555', 'testing this to see what kind of error I am re']
+# sampleEmailAddresses = 'This string contains several test@gmail.com and yahoo@yahoo.com email addresses. I am testing to make sure single @ and .com are not caught as well as yahoo@ and @blank.com are not captured. If this works properly then testing@aol.com will be captured. And everything in "thisEmail@email.com" will also be captured.'
 
 phoneNumRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\)|\[\d{3}\])?    # Area code (optional so uses ?)
@@ -31,60 +27,26 @@ emailRegex = re.compile(r'''(
     )''', re.X)
 
 
-
-# TODO: Find matches in clipboard text
 copiedText = str(pyperclip.paste())
 mo = phoneNumRegex.findall(copiedText)
 mo2 = emailRegex.findall(copiedText)
 numsAndEmails = []
+copiedMessage = 'The following phone numbers and email addresses were copied to your clipboard:'
+noMatchesMessage = 'No phone numbers or email addresses were found.'
 
-# for i in range(len(mo2)):
-#     # print(mo2[i][0])
-#     numsAndEmails.append(str(mo2[i][0]))
-#
-# for i in range(len(mo)):
-#     # print(mo2[i][0])
-#     numsAndEmails.append(str(mo[i][0]))
-#
-# # print(numsAndEmails)
+
 for groups in mo:
     # Gathers and formats phone numbers according to one set standard.
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
     if groups[8] != '':
         phoneNum += f' x{groups[8]}'
     numsAndEmails.append(phoneNum)
+for groups in mo2:
+    numsAndEmails.append(groups[0])
 
-# # TODO: Copy results to the clipboard
-#
-# pyperclip.copy(str(numsAndEmails))
-
-
-# mo = phoneNumRegex.findall(str(samplePhoneNumbers))
-# print(type(mo))
-# # print(type(mo) == list)
-# if type(mo) == list:
-#     phoneNumberList = ''
-# for i in mo:
-#     phoneNumberList += f'{i[0]}\n'
-# print(phoneNumberList) else:
-# print(mo)
-
-# mo = phoneNumRegex.finditer(str(samplePhoneNumbers))
-# print(type(str(samplePhoneNumbers)))
-# print(mo['match'])
-
-# phoneNumRegex = re.compile(r'((\d{3}|\(\d{3}\)|\[\d{3}\])?(\s|-|\.)?(\d{'
-#                            r''r'3})(\s|-|\.)(\d{4}))', re.VERBOSE)
-# mo = phoneNumRegex.findall(str(samplePhoneNumbers))
-# # print(type(str(samplePhoneNumbers)))
-# # print(type(mo))
-# # for i in range(len(mo)):
-# #     print(mo[i][0])
-# print(list(filter(phoneNumRegex.match, samplePhoneNumbers)))
-
-# for i in samplePhoneNumbers:
-#     mo = phoneNumRegex.findall(i)
-#     print(mo)
-#     # for item in mo:
-#     #     print(item[0])
-# print(samplePhoneNumbers)
+if len(numsAndEmails) > 0:
+    pyperclip.copy('\n'.join(numsAndEmails))
+    print(copiedMessage)
+    print(pyperclip.paste())
+else:
+    print(noMatchesMessage)
