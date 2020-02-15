5151195d5cc03975aacc4cbb84e0a29bf90cf4da
TJ Hooker <tyrelhooker@gmail.com>
2020-02-15 11:33:35 -0500

    Work on issues with import statment in pycharm IDE

--- stateCapitalQuizGenerator/stateCapitalQuizGenerator.py
+++ stateCapitalQuizGenerator/stateCapitalQuizGenerator.py
@@ -1,4 +1,15 @@
 #! /usr/bin/python3
 
 import random
-import mv
+import os
+import statedata
+import pkgutil
+import sys
+
+print(statedata.us_states)
+search_path = ['.']
+all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
+print(all_modules)
+
+
+print(sys.path)
