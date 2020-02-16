7c02983f7cd04383654e20da5e6b0b41a43e47ca
TJ Hooker <tyrelhooker@gmail.com>
2020-02-16 00:16:43 -0500

    Create question loop for 50 states and corresponding correct and incorrect answer choices. Write results to txt file.

--- stateCapitalQuizGenerator/stateCapitalQuizGenerator.py
+++ stateCapitalQuizGenerator/stateCapitalQuizGenerator.py
@@ -17,35 +17,50 @@ for i in states_and_capitals:
 # print(capitals)
 
 # Make a new directory on computer
-if not os.path.exists('/home/tj/Documents/automateTheBoring/capitalQuizzes'):
-    os.makedirs('/home/tj/Documents/automateTheBoring/capitalQuizzes')
+if not os.path.exists('/home/tj/Documents/capitalQuizzes'):
+    os.makedirs('/home/tj/Documents/capitalQuizzes')
 
 for quiz_num in range(3):
     # TODO: Create quiz and answer key
-    quiz = open(f'/home/tj/Documents/automateTheBoring/capitalQuizzes/quiz_{quiz_num}', 'w')
-    answer_key = open(f'/home/tj/Documents/automateTheBoring/capitalQuizzes/answer_key_{quiz_num}', 'w')
+    quiz = open(f'/home/tj/Documents/capitalQuizzes/quiz_{quiz_num}', 'w')
+    answer_key = open(f'/home/tj/Documents/capitalQuizzes/answer_key_{quiz_num}', 'w')
 
-    quiz.write(f'Capital Quiz version: {quiz_num}')
+    # TODO: Create Header for the Quiz    quiz.write(f'Capital Quiz version: {quiz_num}')
     answer_key.write(f'Answer Key to Capital Quiz version: {quiz_num}')
 
+    # TODO: Shuffle the order of the states
     states = list(capitals.keys())
     random.shuffle(states)
-    print(states)
+    # print(states)
 
-    # TODO: Create Header for the Quiz
-    # TODO: Shuffle the order of capitals
+    # TODO: Loop through all 50 states, and make a question for each
+    for question_num in range(50):
+        correct_answer = capitals[states[question_num]]
+        # print(type(correct_answer))
+        wrong_answers = list(capitals.values())
+        del wrong_answers[wrong_answers.index(correct_answer)]
+        rand_wrong_answers = random.sample(wrong_answers, 3)
+        answer_choices = rand_wrong_answers + [correct_answer]
+        random.shuffle(answer_choices)
+
+
     # TODO: Create test code to ask 'What is the capital of <state>?' for all 50 states
-    # for state in capitals:
-    #     # print(state)
-    #     capital = capitals[state]
-    #     choices = []
-    #     print(f'What is the capital of {state}?')
-    #     choices.append(capital)
-    #     for wrong_choices in range(1, 4):
-    #         choices.append(random.capitals.values())
+        question = f'\n {question_num + 1}. What is the capital of {states[question_num]}?'
+        answer_letters = ['(a): ', '(b): ', '(c): ', '(d): ']
+        answers = ''
+        for choices in range(len(answer_letters)):
+            answers += f'{answer_letters[choices]} {answer_choices[choices]} \n'
+            # answer_choice_bank.append(answer_letters[choices] + answer_choices[choices])
 
-    # TODO: Create code for 4 multiple choice answers a-d 1 being right and 4 wrong
+
 
     # TODO: Write the results to a txt document
+        quiz.write(
+            question + '\n' + answers
+        )
+        answer_key.write(
+            question + '\n' + correct_answer
+        )
+
     quiz.close()
     answer_key.close()
