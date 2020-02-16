d8b92ee2a85a685e55432a9fbf20fec4a9b1a92e
TJ Hooker <tyrelhooker@gmail.com>
2020-02-15 23:17:17 -0500

    Create os.path.exits check to see if directory exists. Create quiz and answer key txt. Create states shuffle.

--- stateCapitalQuizGenerator/stateCapitalQuizGenerator.py
+++ stateCapitalQuizGenerator/stateCapitalQuizGenerator.py
@@ -7,13 +7,45 @@ import pkgutil
 import sys
 
 all_states_info = statedata.us_states
-state_and_capital = {}
-# print(all_states_info)
+capitals = {}
 
-allVals = all_states_info.values()
-# print(allVals)
-for i in allVals:
-    state_and_capital[i['name']] = i['capital']
-print(state_and_capital)
-# for i in range(len(allVals)):
-#     print(allVals['name'], allVals['capital'])
+
+states_and_capitals = all_states_info.values()
+
+for i in states_and_capitals:
+    capitals[i['name']] = i['capital']
+# print(capitals)
+
+# Make a new directory on computer
+if not os.path.exists('/home/tj/Documents/automateTheBoring/capitalQuizzes'):
+    os.makedirs('/home/tj/Documents/automateTheBoring/capitalQuizzes')
+
+for quiz_num in range(3):
+    # TODO: Create quiz and answer key
+    quiz = open(f'/home/tj/Documents/automateTheBoring/capitalQuizzes/quiz_{quiz_num}', 'w')
+    answer_key = open(f'/home/tj/Documents/automateTheBoring/capitalQuizzes/answer_key_{quiz_num}', 'w')
+
+    quiz.write(f'Capital Quiz version: {quiz_num}')
+    answer_key.write(f'Answer Key to Capital Quiz version: {quiz_num}')
+
+    states = list(capitals.keys())
+    random.shuffle(states)
+    print(states)
+
+    # TODO: Create Header for the Quiz
+    # TODO: Shuffle the order of capitals
+    # TODO: Create test code to ask 'What is the capital of <state>?' for all 50 states
+    # for state in capitals:
+    #     # print(state)
+    #     capital = capitals[state]
+    #     choices = []
+    #     print(f'What is the capital of {state}?')
+    #     choices.append(capital)
+    #     for wrong_choices in range(1, 4):
+    #         choices.append(random.capitals.values())
+
+    # TODO: Create code for 4 multiple choice answers a-d 1 being right and 4 wrong
+
+    # TODO: Write the results to a txt document
+    quiz.close()
+    answer_key.close()
